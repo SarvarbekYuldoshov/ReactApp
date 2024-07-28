3194de45c115dec9416b804215dc236612392b4c
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-07-28 09:52:32 +0300

    salom

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,22 +1,37 @@
 import React, { useEffect, useState } from 'react'
 import "./Header.css"
 const Header = () => {
-    const [categores,setCategoris] = useState ([])
+    const [categories,setCategories] = useState ([])
     const getCity = () =>{
         axios({
-            url:'https://autoapi.dezinfeksiyatashkent.uz/api/categores',
+            url:'https://autoapi.dezinfeksiyatashkent.uz/api/categories',
             method:'GET',
-        }).then(data=>setCategoris(data?.data?.data)).catch(err=>console.log(err))
+
+        })
+
+        .then(data=>setCategories(data?.data?.data))
+        .catch(err=>console.log(err))
+
     }
+
     useEffect(()=>{
         getCity()
+
     },[])
+
   return (
+
     <div>
       {
-        
+        categories && categories.map((item,index)=>(
+            <div>
+                <h1>{item.name}</h1>
+                <h2>{item.text}</h2>
+            </div>
+        ))
       }
     </div>
+    
   )
 }
 import "./Header.css"
